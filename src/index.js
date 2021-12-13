f23b7325b1f208f3e63690b56761845fc17cf39a
Austin Reilly <areilly@alphaworks.tech>
2021-12-13 09:53:21 -0800

    back to working?

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import FrontPage from './front-page/FrontPage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <FrontPage />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
