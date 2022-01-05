80df5c50d13fc18472acf6b02fbd37963406bfc9
Austin Reilly <areilly@alphaworks.tech>
2022-01-05 09:12:30 -0800

    second path doesn't work implementation seems okay

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route, Routes} from 'react-router-dom';
+import {HashRouter, Routes, Route} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 import './css/index.scss';
 import FrontPage from './front-page/FrontPage';
 
-ReactDOM.render(
-  <HashRouter>
-		<Routes>
-			<Route path='/' element={<FrontPage/>}/>
-		</Routes>
-  </HashRouter>,
-  document.getElementById('root') 
-);
+import TheBar from './the-bar/TheBar';
+
+function Index(){
+	return(
+		<HashRouter>
+			<Routes>
+				<Route exact path='/' element={<FrontPage/>}/>
+				<Route exact path='/the-bar' element={<TheBar/>}/>
+			</Routes>
+		</HashRouter>
+	)
+}
+
+ReactDOM.render(<Index/>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
